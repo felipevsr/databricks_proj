a34d3897d6c95ccb94a1afd3d4d950ffd62fd502
felipevsr30@gmail.com <felipevsr30@gmail.com>
2025-07-12 18:56:57 +0000

    Estruturas - catalogo-databases-volumes

---
+++ Structures/structures_catalog_database_volume.py
@@ -0,0 +1,72 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## CATALOG
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE CATALOG IF NOT EXISTS databricks_proj
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## DATABASES
+
+# COMMAND ----------
+
+databases = ["bronze","silver","gold"]
+
+for db in databases:
+  spark.sql(f""" CREATE DATABASE IF NOT EXISTS databricks_proj.{db} """)
+  print(f"Database databricks_proj.{db} created")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Volumes 
+
+# COMMAND ----------
+
+volumes_worspace = ["raw_json","bronze","silver","gold"]
+for itens in volumes_worspace:
+  spark.sql(f""" CREATE VOLUME IF NOT EXISTS workspace.default.{itens} """)
+  print(f"Volume d workspace.default.{itens} created")
+
+
+# COMMAND ----------
+
+df = spark.read.format('csv').option("header",True).load('dbfs:/databricks-datasets/flights/departuredelays.csv')
+df.write.mode("overwrite").format('delta').save('/Volumes/workspace/default/teste100')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TABLE TEMPO_DIST
+# MAGIC USING DELTA
+# MAGIC AS
+# MAGIC select * from delta.`dbfs:/Volumes/workspace/default/teste100/`
+
+# COMMAND ----------
+
+spark.sql(""" CREATE TABLE IF NOT EXISTS  databricks_proj.bronze.teste2000   
+          USING DELTA 
+          AS
+          select * from delta.`/Volumes/workspace/default/teste100/` """)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC show create table databricks_proj.bronze.teste2000   
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select origin,sum(distance) 
+# MAGIC  from databricks_proj.bronze.teste2000
+# MAGIC  group by all
+
+# COMMAND ----------
+
+## DA ERRO
+# %sql
+# create table if not exists databricks_proj.bronze.born1000  using delta location '/Volumes/workspace/default/teste100/'
